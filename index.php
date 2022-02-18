8a60bb6589fe00146aa108ece4105783826a10bd
GUILHERME LIMA DAVID <guilherme.ldavid@senacsp.edu.br>
2022-02-17 23:10:23 -0200

    aulinha php BALA! vetores e matriz

--- index.php
+++ index.php
@@ -1,8 +1,71 @@
 <?php
 
-echo "caraca mlk que fome"
+//comentário de linha
 
-//for($i = 0; $i < 10; $i++){
-  //  echo "Hello world<br>"
-//}
+/*
+Comentário de bloco
+*/
 
+define('XPTO', 'Valor sempre igual');
+
+$var = "Olá Mundo";
+
+echo XPTO;
+
+
+$num = 2;
+
+$var = '<br>' . "olá mundo! $num"; //o ponto é o perador de concatenação
+
+echo $var;
+
+echo '<br><br><h1>vamos tomar café</h1>';  
+
+
+$domingo = 'Domingo';
+$segunda = 'Segunda-feira';
+$terca   = 'Terça-feira';
+$quarta  = 'Quarta-feira';
+$quinta  = 'Quinta-feira';
+$sexta   = 'Sexta-feira';
+$sabado  = 'Sábado';
+
+$diaSemana = [  'dom' => 'Domingo',
+                2 => 'Segunda-feira',
+                3 => 'Terça-feira',
+                4 => 'Quarta-feira',
+                'qui' => 'Quinta-feira',
+                6 => 'Sexta-Feira',
+                'Sab' => 'Sábado'];
+
+
+echo '<br>Hoje é ' . $diaSemana['Sab']; //exemplo sem interpolação
+
+echo "<br>Hoje é {$diaSemana['dom']}";  //exemplo com interpolação
+
+echo "<br>Hoje é {$diaSemana[3]}";  
+
+
+/*
+$usuario['nome'] = 'Luiz';
+$usuario['senha'] = '123';
+
+*/
+
+$usuario[0] = ['nome' => 'Luiz', 'senha' => '123'];
+$usuario[1] = ['nome' => 'fer', 'senha' => '321'];
+$usuario[2] = ['nome' => 'bono', 'senha' => '541'];
+
+echo "<pre>\n\n";
+
+var_dump($usuario);
+
+
+
+/* echo $var;
+
+var_dump($var); 
+
+phpinfo(); informações importante, como variáveis de ambientes
+
+*/
